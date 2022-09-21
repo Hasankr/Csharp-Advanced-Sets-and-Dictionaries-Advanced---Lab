1426d962180f2305d2ee6ebc0cc8f8f4bc431747
hasankr <hasankr@abv.bg>
2022-09-21 22:01:37 +0300

    added 2,3

---
+++ 02. Average Student Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<decimal>> studentGrades = new Dictionary<string, List<decimal>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                string name = input[0];
+                decimal grade = decimal.Parse(input[1]);
+                if (!studentGrades.ContainsKey(name))
+                {
+                    studentGrades.Add(name, new List<decimal>());
+                }
+                studentGrades[name].Add(grade);
+                
+            }
+            foreach (var item in studentGrades)
+            {
+                Console.Write($"{item.Key} -> ");
+                foreach (var grades in item.Value)
+                {
+                    Console.Write($"{grades:f2} ");
+                }
+                Console.WriteLine($"(avg: {item.Value.Average():f2})");
+            }
+        }
+    }
+}
---
+++ 03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            numbers = numbers.OrderByDescending(x => x).ToList();
+            int count = numbers.Count;
+            if (count > 3)
+            {
+                count = 3;
+            }
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
