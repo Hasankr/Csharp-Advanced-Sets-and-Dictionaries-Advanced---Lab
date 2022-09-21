09f459146967a6652abbce3ec20f220edc1c44cc
hasankr <hasankr@abv.bg>
2022-09-21 20:59:42 +0300

    Add project files.

---
+++ 01. Count Same Values in Array/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Same_Values_in_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+            Dictionary<double, int> numberTimes = new Dictionary<double, int>();
+            foreach (var number in numbers)
+            {
+                if (!numberTimes.ContainsKey(number))
+                {
+                    numberTimes.Add(number, 0);
+                }
+                numberTimes[number]++;
+            }
+            foreach (var item in numberTimes)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
